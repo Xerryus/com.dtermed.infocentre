4cac228c7330e357102468f86fefe3948782403e
XXX <xxx@xxx.com>
2015-03-17 10:19:05 +0100

    Update# 28

--- src/Contents/Application/app/view/VForm.js
+++ src/Contents/Application/app/view/VForm.js
@@ -281,7 +281,7 @@ App.view.define('VForm',
 												{
 													top: 23,
 													//bottom: 5,
-													left: 10
+													left: 30
 													//right: 10
 												}
 										}
