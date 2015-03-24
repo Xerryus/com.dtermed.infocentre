fb2ed0808866a20444f453b3e71de3a03596a2ca
XXX <xxx@xxx.com>
2015-03-24 15:00:22 +0100

    Update# 90

--- src/Contents/Services/TraitementTable.js
+++ src/Contents/Services/TraitementTable.js
@@ -80,7 +80,7 @@ TraitementTable =
 			{
 
 				var db=TraitementTable.using('db');
-				db.query('infocentre2015','update base set service="'+o.service+'", departement="'+o.departement+'", agent_beneficiaire="'+o.agent_beneficiaire+'", nature="'+o.nature+'", sous_nature="'+o.sous_nature+'", evolution="'+o.evolution+'", quantite="'+o.quantite+'", phasage="'+o.phasage+'", libelle_commande="'+o.libelle_commande+'", motivation_demande="'+o.motivation_demande+'", commentaire_demande="'+o.commentaire_demande+'", commentaire_S2i="'+o.commentaire_S2i+'", priorite_niveau="'+o.priorite_niveau+'", special="'+o.special+'", domaine_metier="'+o.domaine_metier+'" where ID_demande="'+o.ID_demande+'" ',cb);
+				db.query('infocentre2015','update base set nature="'+o.nature+'", sous_nature="'+o.sous_nature+'", evolution="'+o.evolution+'", phasage="'+o.phasage+'", libelle_commande="'+o.libelle_commande+'", motivation_demande="'+o.motivation_demande+'", commentaire_demande="'+o.commentaire_demande+'", commentaire_S2i="'+o.commentaire_S2i+'", domaine_metier="'+o.domaine_metier+'" where ID_demande="'+o.ID_demande+'" ',cb);
 			},		
 		acces_infocentre2015_base_lecture: function(o,cb)
 			{
