7c24ec29a58977131db3215d3234f94aed7164e7
XXX <xxx@xxx.com>
2015-03-24 14:06:19 +0100

    Update# 77

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -101,8 +101,8 @@ App.controller.define('CMain',
 							//App.get('VForm',{height:590});
 							//App.get('button#btnajouter').el.setVisibilityMode(Ext.Element.DISPLAY);
 							//App.get('button#btnajouter').hide(true);
-							App.get('button#btnmodifier').el.setVisibilityMode(Ext.Element.DISPLAY);
-							App.get('button#btnmodifier').hide(true);							
+							//App.get('button#btnmodifier').el.setVisibilityMode(Ext.Element.DISPLAY);
+							//App.get('button#btnmodifier').hide(true);							
 							break ;
 						case 2: 
 							Ext.Msg.alert('Omneedia','Profil Chef de Département'); // Affichage du profil
