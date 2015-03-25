8049ec7eac5d521d29939dcd554d542cd776a01c
XXX <xxx@xxx.com>
2015-03-25 12:28:48 +0100

    Update# 167

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -236,7 +236,7 @@ App.controller.define('CMain',
 					if (dat.phasage==2) App.get('radio#RP2').setValue(true);
 					if (dat.phasage==3) App.get('radio#RP3').setValue(true);					
 					App.get('checkbox#chbspecial').setValue(dat.special);
-					console.load(record);
+					console.log(record);
 					
 /* 					store: App.store.create('getAffichegrid01', // Creation du store
 						{
