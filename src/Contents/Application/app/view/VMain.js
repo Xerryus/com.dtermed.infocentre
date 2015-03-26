67223de841beaf4b86b270502613f2baadb91633
XXX <xxx@xxx.com>
2015-03-26 15:12:11 +0100

    Update# 237

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -218,7 +218,7 @@ App.view.define('VMain',
 										},
  										{
 											text: "Evolution", //
-											dataIndex: "libelle_evolution", // Valeur de la table
+											dataIndex: "libelle_evolutionc", // Valeur de la table
 											width: 90
 										},
  										{
