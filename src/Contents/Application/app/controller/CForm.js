4548dc3e857165a02f824299f173716f4714fe30
XXX <xxx@xxx.com>
2015-03-26 12:50:50 +0100

    Update# 217

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -123,6 +123,9 @@ App.controller.define('CForm',
 							//App.get('VForm',{height:490});
 							App.get('panel#regroupement_z1').el.setVisibilityMode(Ext.Element.DISPLAY);
 							App.get('panel#regroupement_z1').hide(true);
+							// Cache regroupement_z9
+							App.get('panel#regroupement_z9').el.setVisibilityMode(Ext.Element.DISPLAY);
+							App.get('panel#regroupement_z9').hide(true);							
 							// efface le bouton enregistrer
 							App.get('button#btnenregistrer').el.setVisibilityMode(Ext.Element.DISPLAY);
 							App.get('button#btnenregistrer').hide(true);
