3148a112853a8e9794aadb6d0630aa9a6f83f9a8
XXX <xxx@xxx.com>
2015-03-27 14:22:49 +0100

    Update# 283

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -374,21 +374,18 @@ App.controller.define('CForm',
 				//Ext.Msg.alert('Omneedia',o.nature+" "+o.sous_nature);
 				//Ext.Msg.alert('Omneedia',o.nature+" "+o.sous_nature+" "+profil);
 				
-				if (profil==1)
-					{
 						if (App.get('combo#cbo1').getValue() === null)
 							{
 								Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Département !');
-							}
+							} else
 						if (App.get('combo#cbo2').getValue()=== null)
 							{
 								Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Service !');
-							}
+							} else
 						if(App.get('combo#cbo3').getValue()=== null)
 							 {
 								Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Bénéficaire !'); 
 							 }
-					}
 				
 				Ext.Msg.alert('Omneedia',o.departement+" "+o.service+" "+o.agent_beneficiaire);
 				//App.get('VForm').close();
