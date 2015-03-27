ab5204330133da9cc9f6a96f9d09c66203d95534
XXX <xxx@xxx.com>
2015-03-27 14:03:23 +0100

    Update# 274

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -379,8 +379,8 @@ App.controller.define('CForm',
 						if (App.get('combo#cbo1').getValue()=='null' || 'undefined')
 						{
 						 	Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Département !');
-						} else 
-							if(App.get('combo#cbo2').getValue()=='null')
+						} 
+						if (App.get('combo#cbo2').getValue()=='null' || 'undefined' )
 								{
 									Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Service !');
 								} else 
