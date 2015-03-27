8bc2ed2f6bd39a48dd48aa95db87ed1f68de684d
XXX <xxx@xxx.com>
2015-03-27 14:12:12 +0100

    Update# 279

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -376,15 +376,15 @@ App.controller.define('CForm',
 				
 				if (profil==1)
 					{
-						if (App.get('combo#cbo1').getValue() == null || undefined )
+						if (App.get('combo#cbo1').getValue() === null)
 						{
 						 	Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Département !');
 						} 
-						if (App.get('combo#cbo2').getValue()== null || undefined )
+						if (App.get('combo#cbo2').getValue()=== null)
 								{
 									Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Service !');
 								} 
-									if(App.get('combo#cbo3').getValue()=='null')
+									if(App.get('combo#cbo3').getValue()==='null')
 									 {
 										Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Bénéficaire !'); 
 									 }
