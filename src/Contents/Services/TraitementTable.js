6b80f506bf935f2f208fb30b05e1b7332c4e72eb
XXX <xxx@xxx.com>
2015-03-27 12:52:46 +0100

    Update# 240

--- src/Contents/Services/TraitementTable.js
+++ src/Contents/Services/TraitementTable.js
@@ -74,7 +74,7 @@ TraitementTable =
 		acces_infocentre2015_base_update: function(o,cb)
 			{
 				var db=TraitementTable.using('db');
-				db.query('infocentre2015','update base set phasage="'+o.phasage+'",priorite_valide="'+o.priorite_valide+'", etape_valide="'+o.etape_valide+'", avancement="'+o.avancement+'", annulation="'+o.annulation+'", special="'+o.special+'" where ID_demande="'+o.ID_demande+'" ',cb);	
+				db.query('infocentre2015','update base set phasage="'+o.phasage+'",priorite_valide="'+o.priorite_valide+'", etape_valide="'+o.etape_valide+'", avancement="'+o.avancement+'", annulation="'+o.annulation+'", special="'+o.special+'", ,date_modif="'+o.date_modif'" where ID_demande="'+o.ID_demande+'" ',cb);	
 			},
 		acces_infocentre2015_base_modifier: function(o,cb)
 			{
