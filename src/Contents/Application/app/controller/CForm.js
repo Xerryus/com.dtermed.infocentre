f3b48231a57a77484b5211737e4fc31d29e360fc
XXX <xxx@xxx.com>
2015-03-30 08:04:09 +0200

    Update# 317

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -339,6 +339,7 @@ App.controller.define('CForm',
 				if ( (App.get('combo#cbo1').getValue() == null) || (App.get('combo#cbo2').getValue() == null) || (App.get('combo#cbo3').getValue() == null))
 					{
 						Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Département !');
+						Ext.Msg.alert('Omneedia',App.get('combo#cbo1').getValue()+" "+App.get('combo#cbo2').getValue()+" "+App.get('combo#cbo3').getValue());
 						//break ;
 					} else
 					{
