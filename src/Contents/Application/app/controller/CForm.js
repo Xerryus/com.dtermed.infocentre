aedf631d30a137d5fd2b6d52d9acddea809c7171
XXX <xxx@xxx.com>
2015-03-30 09:44:36 +0200

    Update# 344

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -332,27 +332,27 @@ App.controller.define('CForm',
 			},
 		btnenregistrer_onclik: function(p) // pour créer un nouvel enregistrement de demande
 			{
-				if ( App.get('combo#cbo1').getValue() == 'null' )
+				if ( App.get('combo#cbo1').getValue() == null )
 					{
 						alert("Veuillez saisir le Département svp !");
 						return false;
 					}					
-				if ( App.get('combo#cbo2').getValue() == 'null' )
+				if ( App.get('combo#cbo2').getValue() == null )
 					{
 						alert("Veuillez saisir le Service svp !");
 						return false;						
 					}					
-				if ( App.get('combo#cbo3').getValue() == 'null' )
+				if ( App.get('combo#cbo3').getValue() == null )
 					{
 						alert("Veuillez saisir le Bénéficiaire svp !");
 						return false;						
 					}					
-				if ( App.get('combo#cbo6').getValue() == 'null' )
+				if ( App.get('combo#cbo6').getValue() == null )
 					{
 						alert("Veuillez saisir le Domaine métier svp !");
 						return false;						
 					}				
-				if ( App.get('combo#cbo4').getValue() == 'null' )
+				if ( App.get('combo#cbo4').getValue() == null )
 					{
 						alert("Veuillez saisir la Nature svp !");
 						return false;						
