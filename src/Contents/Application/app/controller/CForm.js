fcdebbebada1861ce99adae308713d0887334649
XXX <xxx@xxx.com>
2015-03-30 08:01:36 +0200

    Update# 315

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -335,11 +335,12 @@ App.controller.define('CForm',
 				var controle = false;
 				
 
-				//Ext.Msg.alert('Omneedia',App.get('combo#cbo1').getValue()+" "+App.get('combo#cbo2').getValue()+" "+App.get('combo#cbo3').getValue());
+				Ext.Msg.alert('Omneedia',App.get('combo#cbo1').getValue()+" "+App.get('combo#cbo2').getValue()+" "+App.get('combo#cbo3').getValue());
 				
 				if ( (App.get('combo#cbo1').getValue() == null) || (App.get('combo#cbo2').getValue() == null) || (App.get('combo#cbo3').getValue() == null))
 					{
-						Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Département !');													
+						Ext.Msg.alert('Erreur', 'Veuillez selectionner une valeure pour Département !');
+						break ;
 					} else
 					{
 						Ext.Msg.alert('OK', 'Sauvegarde en cours !');
