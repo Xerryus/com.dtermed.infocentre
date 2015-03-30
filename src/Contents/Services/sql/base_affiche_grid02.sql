37a4472f347cd8846c551f3f21ffe172ea01ba52
XXX <xxx@xxx.com>
2015-03-30 15:02:52 +0200

    Update# 369

---
+++ src/Contents/Services/sql/base_affiche_grid02.sql
@@ -0,0 +1,12 @@
+select *, concat(Nom," ",Prenom) NomPre 
+	from base as b 
+	join domaine_metier as dm on b.domaine_metier = dm.ID_domaine_metier 
+	join nature as n on b.nature = n.ID_nature 
+	join sous_nature as sn on b.sous_nature = sn.ID_sous_nature 
+	join priorite as p on b.priorite_niveau = p.ID_priorite 
+	join evolution as ev on b.evolution = ev.ID_evolution 
+	join priorite as pr on b.phasage = pr.ID_priorite 
+	join bpclight.agents as ka on b.agent_beneficiaire = ka.Kage 
+	join bpclight.unites as ku on b.departement = ku.Kuni 
+	join bpclight.subdis as su on b.service = su.Ksub 
+order by date_de_demande desc where ID_annee_budgetaire='+o.id'
