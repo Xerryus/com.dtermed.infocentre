1579165d2a925faed3c907660a379b6a118f50b6
XXX <xxx@xxx.com>
2015-04-01 08:21:24 +0200

    Update# 394

--- src/Contents/Application/app/view/VForm.js
+++ src/Contents/Application/app/view/VForm.js
@@ -274,7 +274,7 @@ App.view.define('VForm',
 												{
 													top		: 3,
 													bottom	: 0,
-													left	: 10,
+													left	: 10, // 20
 													right	: 10
 												},
 											border	: false,
