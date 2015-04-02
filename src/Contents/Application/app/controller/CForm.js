245c81cff7e60a25a64bb750bb22ca637330cae8
XXX <xxx@xxx.com>
2015-04-02 15:13:28 +0200

    Update# 456

--- src/Contents/Application/app/controller/CForm.js
+++ src/Contents/Application/app/controller/CForm.js
@@ -98,7 +98,7 @@ App.controller.define('CForm',
 							}
 					});
 				dat="";
-				var textsaisimanuellement = 'null';
+				var textsaisimanuellement = null;
 				var flag_saisimanuellement = -1;
 			},
 			
