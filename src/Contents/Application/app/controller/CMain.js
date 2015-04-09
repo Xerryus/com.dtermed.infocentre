33d7ebdc6d7b3fc6d6b8ea7ffb1e2aca42f1dbad
XXX <xxx@xxx.com>
2015-04-09 10:31:23 +0200

    Update# 485

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -201,15 +201,9 @@ App.controller.define('CMain',
 					App.get('combo#cbo1').setValue(dat.departement); // Departement
 					App.get('combo#cbo2').setValue(dat.LibSubC); // Service
 					
-					if ( App.get('combo#cbo3').getValue() == null )
-						{
-							App.get('combo#cbo3').setValue('Autre'); // Beneficiaire
-						} else 
-							//if ( App.get('combo#cbo3').getValue() != null ) 
-						{ 
-							App.get('combo#cbo3').setValue(dat.NomPre); // Beneficiaire
-						};
-								
+					if ( App.get('combo#cbo3').getValue() == null )	App.get('combo#cbo3').setValue('Autre'); // Beneficiaire 
+					if ( App.get('combo#cbo3').getValue() != null ) App.get('combo#cbo3').setValue(dat.NomPre); // Beneficiaire
+
 					console.log(App.get('combo#cbo3').getValue());
 					
 					App.get('combo#cbo6').setValue(dat.ID_domaine_metier); // Domaine metier
