a48dcb4c61c42f15adaaaabb52cf4fc52b8017c0
XXX <xxx@xxx.com>
2015-04-15 11:08:05 +0200

    Update# 547

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -165,10 +165,10 @@ App.controller.define('CMain',
 		
 	cboDepartementFiltre_onclik : function()
 		{
-				var id=App.get('combo#cboDepartementFiltre').getValue();
+/* 				var id=App.get('combo#cboDepartementFiltre').getValue();
 				App.get('combo#ServiceFiltre').setValue(null);
 				App.get('combo#ServiceFiltre').getStore().getProxy().extraParams.id=id;
-				App.get('combo#ServiceFiltre').getStore().load();
+				App.get('combo#ServiceFiltre').getStore().load(); */
 		},
 		
 	cboServiceFiltre_onclick : function()
