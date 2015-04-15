7de3a6f181c04aff14b9ebdd4550cd59ae61ea0a
XXX <xxx@xxx.com>
2015-04-15 14:00:37 +0200

    Update# 561

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -221,6 +221,9 @@ App.controller.define('CMain',
 					App.get('combo#cbo1').setValue(dat.departement); // Departement
 					App.get('combo#cbo2').setValue(dat.LibSubC); // Service
 					
+					App.get('combo#cboDepartementFiltre').setValue(dat.departement); // Departement
+					App.get('combo#cboServiceFiltre').setValue(dat.LibSubC); // Service					
+					
 					if ( App.get('combo#cbo3').getValue() != 0 ) App.get('combo#cbo3').setValue(dat.NomPre); // Beneficiaire
 					if ( App.get('combo#cbo3').getValue() == false )	App.get('combo#cbo3').setValue('Autre-->commentaire'); // Beneficiaire
 					
